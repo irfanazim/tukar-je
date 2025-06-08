b4ee415c8627eab01893ed2be671d57b9c039109
Azim <aziimhilmi@gmail.com>
2025-06-08 11:40:39 +0800

    Fixes for deployment (2)

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from app import create_app, db
 
 app = create_app()
 
+# Create database tables
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     # For production deployment
     port = int(os.environ.get('PORT', 10000))
