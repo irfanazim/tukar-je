f669f6e693da269d05cd539bd921f4665c2cd390
Azim <aziimhilmi@gmail.com>
2025-06-12 21:37:06 +0800

    Minor changes for room_info and deployment process

--- run.py
+++ run.py
@@ -8,6 +8,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    # For production deployment
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port) 
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True) 
